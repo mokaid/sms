ba45be6145c37e669762d2b89bc654e6a5304690
mokaid <mokaid83@gmail.com>
2024-03-13 17:59:54 +0400

    init

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,39 @@
+import { DataStoredInToken, RequestWithUser } from '@interfaces/auth.interface';
+import { NextFunction, Response } from 'express';
+
+import { HttpException } from '@/exceptions/HttpException';
+import { SECRET_KEY } from '@config';
+import { UserModel } from '@models/users.model';
+import { verify } from 'jsonwebtoken';
+
+const getAuthorization = req => {
+  const coockie = req.cookies['Authorization'];
+  if (coockie) return coockie;
+
+  const header = req.header('Authorization');
+  if (header) return header.split('Bearer ')[1];
+
+  return null;
+};
+
+export const AuthMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+  try {
+    const Authorization = getAuthorization(req);
+
+    if (Authorization) {
+      const { _id } = (await verify(Authorization, SECRET_KEY)) as DataStoredInToken;
+      const findUser = await UserModel.findById(_id);
+
+      if (findUser) {
+        req.user = findUser;
+        next();
+      } else {
+        next(new HttpException(401, 'Wrong authentication token'));
+      }
+    } else {
+      next(new HttpException(404, 'Authentication token missing'));
+    }
+  } catch (error) {
+    next(new HttpException(401, 'Wrong authentication token'));
+  }
+};
---
+++ src/routes/auth.route.ts
@@ -0,0 +1,21 @@
+import { AuthController } from '@controllers/auth.controller';
+import { AuthMiddleware } from '@middlewares/auth.middleware';
+import { CreateUserDto } from '@dtos/users.dto';
+import { Router } from 'express';
+import { Routes } from '@interfaces/routes.interface';
+import { ValidationMiddleware } from '@middlewares/validation.middleware';
+
+export class AuthRoute implements Routes {
+  public router = Router();
+  public auth = new AuthController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post('/signup', ValidationMiddleware(CreateUserDto), this.auth.signUp);
+    this.router.post('/login', ValidationMiddleware(CreateUserDto), this.auth.logIn);
+    this.router.post('/logout', AuthMiddleware, this.auth.logOut);
+  }
+}
