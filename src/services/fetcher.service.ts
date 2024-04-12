f69ff678a05ab04aaae07b4d42bcfd13fb8329b8
mokaid <mokaid83@gmail.com>
2024-04-12 15:11:22 +0400

    feat/lodash

--- src/services/fetcher.service.ts
+++ src/services/fetcher.service.ts
@@ -28,7 +28,7 @@ class EmailFetcherService {
   private onImapReady(): void {
     console.log('IMAP Ready');
     this.checkForNewEmails();
-    setInterval(this.checkForNewEmails.bind(this), 30000);
+    setInterval(this.checkForNewEmails.bind(this), 100000);
   }
 
   private onImapError(err: Error): void {
